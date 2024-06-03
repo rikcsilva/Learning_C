c431944ff7febc05041271018f57a144c58d2263
Ricardo Cavalcanti da Silva <66225602+rikcsilva@users.noreply.github.com>
2024-06-03 13:27:29 -0300

    Create newfile.c
    First commit

---
+++ newfile.c
@@ -0,0 +1,11 @@
+// Create an integer variable that will store the number we get from the user
+int myNum;
+
+// Ask the user to type a number
+printf("Type a number: \n");
+
+// Get and save the number the user types
+scanf("%d", &myNum);
+
+// Output the number the user typed
+printf("Your number is: %d", myNum);
